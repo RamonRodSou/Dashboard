e8b97370a50213f30f07cd628a8685a5107fc552
Ramon Rodrigues <ramonrodsouza@gmail.com>
2024-08-17 17:04:54 -0300

    feat: foi criado o nav mobile e desktop, e agora criando cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import SideBar from "@/components/SideBar/SideBar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased", inter.className
+      )}>
+
+        <SideBar/>
+        {children}</body>
     </html>
   );
 }
